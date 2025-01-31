b8ad75b072f58bc48710ea7602d7a1baf3817aa5
MELCHIOR OMOTOSHO B. BANKOLE <melchior.bankole@chem.loc>
2025-01-31 12:02:55 +0100

    initialisation et configuration du projet

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1 class="text-red-200">Hello sur WallSocket</h1>
+
+<UserPen />
+    </div>
+</template>
+
+<script setup lang="ts">
+import {UserPen} from 'lucide-vue-next';
+
+</script>
